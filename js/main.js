7432a0c20d15903068ef8e72736ff6f629ca1753
strafe <dgulkovskiy@gmail.com>
2013-12-22 13:53:09 +0200

    karkas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    $('#nav li').not('.active').hover(highlight);
+    $('#user').click(ddown);
 });
+function highlight(){
+    $(this).toggleClass('active');
+}
+function ddown(){
+    $('.ddown-cont').slideToggle();
+}
